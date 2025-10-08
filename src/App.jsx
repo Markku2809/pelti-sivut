55cea92275d30cfb03fce2169a4a71974b63cc6b
Markku2809 <markkuinvest@gmail.com>
2025-10-08 13:28:36 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Pelti-sivut</h1>
+      <p>Tämä on testisivu. Build toimii 🎉</p>
+    </div>
+  );
+}
