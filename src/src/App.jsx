aeb7f64d1334683f5d596f33939957b02d51573b
Markku2809 <markkuinvest@gmail.com>
2025-10-08 12:32:28 +0300

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductsNO from "./ProductsNO.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, maxWidth: 900, margin: "0 auto" }}>
+      <h1>Nordic Sheet Metal</h1>
+      <ProductsNO />
+    </div>
+  );
+}
